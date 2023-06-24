9c2ea332fdb4c93a83964c2e181c04716fa7a9c3
akadiric <akadiric@sfu.ca>
2023-06-24 16:03:06 -0700

    built bones of videoplayer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./styles/App.css";
+import "./styles/General.css";
+import placeholderimage from "./media/placeholder-picture.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<div class="embed-responsive embed-responsive-16by9">
-						<iframe
-							width="560"
-							height="315"
-							src="https://www.youtube.com/embed/videoseries?list=PLUt2ZbkV3lGG_ys0_GxDOQmgLHi6-VZyY"
-							title="YouTube video player"
-							frameborder="0"
-							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-						></iframe>
-					</div>
-				</a>
-			</header>
-		</div>
+			<div className="video-player-background">
+				<div className="current-video">
+					{/* add details for text here*/}
+					<iframe className="current-youtube-video"
+						src="https://www.youtube.com/embed/videoseries?list=PLUt2ZbkV3lGG_ys0_GxDOQmgLHi6-VZyY"
+						title="YouTube video player"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					></iframe>
+					<div className="white-line"></div>
+					<div className="white-line"></div>
+					{/* add details for timer here */}
+				</div>
+				<div className="scenes-grid">
+					{/* change these areas to be stills from the scene. not sure hwo to go about this*/}
+					<img className="test-current-youtube-video" src={placeholderimage}></img>
+					<img className="test-current-youtube-video" src={placeholderimage}></img>
+					<img className="test-current-youtube-video" src={placeholderimage}></img>
+					<img className="test-current-youtube-video" src={placeholderimage}></img>
+					<img className="test-current-youtube-video" src={placeholderimage}></img>
+				</div>
+				<div className="bottom-content">
+					<div className="white-line"> </div>
+					<p className="bottom-text"> NCIC [XREF.a]</p>
+					{/* add details for text here*/}
+				</div>
+				</div>
 	);
 }
 
